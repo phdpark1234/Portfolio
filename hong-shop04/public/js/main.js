1b4b481abac0f9ef1e92689f0c3a2497e39bf4f1
hongik710 <hongik710@hanmail.net>
2019-04-09 05:58:14 +0900

    main.js슬라이드 할 차례

--- hong-shop04/public/js/main.js
+++ hong-shop04/public/js/main.js
@@ -75,20 +75,44 @@ function homeAdd(data) {
 function blogAdd(data) {
     var html = '';
     html += '<ul id="'+data.key+' class="grid-item">';
-    html += '<li class="grid-tit">'+data.val().name+'</li>';
+    html += '<li class="grid-item">'+data.val().name+'</li>';
     html += '</ul>';
     $(".grid").append(html);
     db.ref("root/blog/"+data.key+"/sub").once("value", function(sub){
         sub.forEach(function(v, i){
             html = '<li class="rt_arrow" id="'+v.key+'>';
             html += '<a href="'+v.val().link+' target="'+v.val().target+'>'+v.val().name+'</a>';
-            html += '</li>';
+            html +='</li>';
             $("#"+data.key).append(html);
         });
     });
 }
 
 //카테고리 SHOP 생성 - Ajax/json 통신
+new Ajax("../json/shop.json", shopAjax);
+function shopAjax(data) {
+    var html = '<div class="shop_cates wrap clear">';
+    for(var i=0; i<data.cates.length; i++) {
+        html += '<ul>';
+        html += '<li class="shop_cate_tit">'+data.cates[i].tit+'</li>';
+        html += '<li>';
+        html += '<ul>';
+        for(var j=0; j<data.cates[i].data.length; j++) {
+        html += '<li class="shop_cate_name rt_arrow">';
+        html += '<a href="'+data.cates[i].data[j].link+' target="'+data.cates[i].data[j].target+'>'+data.cates[i].data[j].name+'</a></li>';    
+        }
+        html += '</ul></li></ul>';
+    }
+    html += '</div>';
+    html += '<ul class="shop_prds">';
+    for(i=0; i<data.prds.length; i++) {
+        html += '<li class="shop_prd ovhide"><a href="'+data.prds[i].link+' target="'+data.prds[i].target+'><img src="'+data.prds[i].src+' class="img  size_ani"></a></li>';
+    }
+    html += '</ul>';
+    $(".nav_sub").eq(1).append(html);
+}
+
+//카테고리 PORTFOLIO 생성 - Ajax/json 통신
 new Ajax("../json/port.json", portAjax);
 function portAjax(data) {
     for(var i in data.ports) {
@@ -99,11 +123,69 @@ function portAjax(data) {
 //메인 좌측 네비 - lefts - Ajax/json 통신
 new Ajax("../json/left.json", leftAjax);
 function leftAjax(data) {
-    var html;
+    var html; 
     for(var i in data.lefts) {
         html = '<li class="rt_arrow">'+data.lefts[i].name+'</li>';
         $(".left").append(html);
     }
 }
 
-// window.resize()구현
+//window,resize()구현 
+$(window).resize(function(){
+
+}).trigger("resize");
+
+//top_nav hover 이벤트
+$(".top_icon").mouseenter(function(){
+    $(this).children("img").css({"opacity":.7});
+});
+$(".top_icon").mouseleave(function(){
+    $(this).children("img").css({"opacity":1});
+});
+
+// nav 이벤트 (nav_sub show/hide)
+$(".nav").mouseenter(function(){
+    $(this).children(".nav_sub").css({"display":"block", "opacity":0}).stop().animate({"opacity":1, "top":"45px"}, 200);
+});
+$(".nav").mouseleave(function(){
+    $(this).children(".nav_sub").stop().animate({"opacity":0, "top":"80px"}, 200, function(){
+        $(this).css({"display":"none"});
+    });
+});
+
+
+//메인배너 / .bans
+fadeShow();
+function fadeShow() {
+    var $wrap = $(".ban");
+    var $slide = $(".ban > li");
+    var depth = 10;
+    var now = 0;
+    var speed = 500;
+    var timeout = 3000;
+    var end = $slide.length - 1;
+    var interval;
+    var hei;
+    //Pager 초기화
+    $slide.each(function(){
+        $(this).css({"position":"absolute", "top":0});
+        $(".cycle-pager").append("<span>●</span>");
+    });
+    $(".cycle-pager span").click(function(){
+        now = $(this).index();
+        fadeAni();
+        clearInterval(interval);
+        interval = setInterval(fadeAni, timeout);
+    });
+    $(".bans").height($slide.eq(0).height());
+    $(window).resize(function(){
+        $(".bans").height($slide.eq(now).height());
+    });
+    //최초 실행
+    interval = setInterval(fadeAni, timeout);
+    function fadeAni() {
+        $(window).trigger("resize");
+        $(".cycle-pager span").removeClass("cycle-pager-active");
+        $(".cycle-pager span").eq(now).addClass("cycle-pager-active");
+    }
+}
